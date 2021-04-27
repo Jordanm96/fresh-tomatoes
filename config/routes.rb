70900f7aa6693b3b68a30008e78550ff2c49df6f
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-04-26 20:55:19 -0700

    Added show movie route to config/routes and set up some services folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
-  get '/auth/verify', to: 'authentication#verify' 
-  
+  get '/auth/verify', to: 'authentication#verify'
+  # In the video, he has :id after reviews. Why don't we have to use :review_id?
+  get '/movies/:movie_id/reviews/:id', to: 'movies#show'
+  get '/movies/:movie_id/reviews/:review_id', to: 'movies#show'
   resources :reviews, only: [:create, :destroy]
-  resources :movies, except: :destroy
+  resources :movies, except: [:destroy, :show]
   resources :users, only: :create
 end
