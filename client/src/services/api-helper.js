5172195af0ec18698b3ec8a2ff7b4c9ddf8e8298
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-04-29 14:17:53 -0700

    deploying

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
 // If I want to use netlify, use the one below
-// const baseUrl = process.env.NODE_END === 'production' ? 'https://fresh-tomatoes-api.herokuapp.com/' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_END === 'production' ? 'https://fresh-tomatoes-api.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
