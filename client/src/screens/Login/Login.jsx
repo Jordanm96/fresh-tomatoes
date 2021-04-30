948e975f5ebb1dbf1403bdd5cd9a29e8fd167442
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-04-30 10:44:39 -0700

    Login form 80% done

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Form } from "react-bootstrap";
+
 import './Login.css'
 const Login = (props) => {
   const [formData, setFormData] = useState({
@@ -18,34 +20,42 @@ const Login = (props) => {
   }
 
   return (
-    <div className = 'basic-form'>
+    <>
       <h3>LOG IN</h3>
-      <form onSubmit= {(e) => {
+      <form className='user-form' onSubmit= {(e) => {
         e.preventDefault();
         handleLogin(formData);
       }}>
-        <label>
+        <div className='inputs'>
+
+        <label className= 'login'>
           Username:
+          </label>
           <input
             type='text'
             name='username'
             value={username} 
-            onChange={handleChange} 
-          />
-        </label>
-        <label>
+            onChange={handleChange}
+            className= 'login'
+            />
+        <label className= 'login'>
           Password:
+          </label>
           <input
             type='password'
             name='password'
             value={password}
-            onChange={handleChange} 
-          />
-        </label>
-        <button>GO</button>
+            onChange={handleChange}
+            className= 'login'
+            />
+            </div>
+          <button>GO</button>
+          <label className= 'link'>Don't have an account? {" "}
+
         <Link to='/register'>REGISTER HERE</Link>
+          </label>
       </form>
-    </div>
+    </>
   );
 };
 
